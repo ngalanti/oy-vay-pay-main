d13b5e46539a1b15f5e730acdb923d28f59bcb8b
ngalanti <galanti.nehoray@gmail.com>
2025-02-25 11:21:56 +0200

    first commit

---
+++ client/src/api/income.js
@@ -0,0 +1,68 @@
+import api from "./api";
+
+export const createIncomes = async (payload) => {
+  try {
+    const { userId } = payload;
+    const { data } = await api.post(`/add-income/${userId}`, payload);
+
+    return data;
+  } catch (error) {
+    const message =
+      error.response?.data.message ||
+      "an error occurred while creating the income please try again";
+
+    throw new Error(message);
+  }
+};
+
+export const getIncomes = async (userId) => {
+  try {
+    const { data } = await api.get(`/get-incomes/${userId}`);
+    return data;
+  } catch (error) {
+    const message =
+      error.response?.data.message ||
+      "an error occurred while fetching the Incomes please try again";
+
+    throw new Error(message);
+  }
+};
+export const getTotalIncomes = async (userId) => {
+  try {
+    const { data } = await api.get(`/get-total-incomes/${userId}`);
+    return data;
+  } catch (error) {
+    const message =
+      error.response?.data.message ||
+      "an error occurred while fetching the total Incomes please try again";
+
+    throw new Error(message);
+  }
+};
+
+export const editIncome = async (userId, incomeId, income) => {
+  try {
+    const { data } = await api.patch(
+      `/update-income/${userId}/${incomeId}`,
+      income
+    );
+    return data;
+  } catch (error) {
+    const message =
+      error.response?.data.message ||
+      "an error occurred while updating incomes please try again";
+    throw new Error(message);
+  }
+};
+
+export const deleteIncome = async (userId, incomeId) => {
+  try {
+    const { data } = await api.delete(`/delete-income/${userId}/${incomeId}`);
+    return data;
+  } catch (error) {
+    const message =
+      error.response?.data.message ||
+      "an error occurred while deleting income please try again";
+    throw new Error(message);
+  }
+};
